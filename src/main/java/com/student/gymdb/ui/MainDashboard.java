6d25f0adf1c5db8392df695f8a2315fd1702fe4f
SevinHawkins <TimeBandit2k@gmail.com>
2026-04-27 15:59:37 -0500

    Removed more not needed comments that my IDE put in here

--- src/main/java/com/student/gymdb/ui/MainDashboard.java
+++ src/main/java/com/student/gymdb/ui/MainDashboard.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package com.student.gymdb.ui;
+
 import com.student.gymdb.dao.MemberDAO;
 import com.student.gymdb.dao.WorkoutLogDAO;
 import com.student.gymdb.model.Member;
